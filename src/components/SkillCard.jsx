cdf64acc141c884b4e278b98a0718a25a29cdf24
Shadowguy01234 <anuragchess22@gmail.com>
2025-06-13 17:49:18 +0530

    Completed Skills page and Updated SkillCard component

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 import { FaNodeJs } from "react-icons/fa";
 
-function SkillCard({icon , title, description}) {
-return (
-    <div className='bg-zinc-800 rounded-lg w-36 h-36 flex justify-center items-center flex-col gap-3 transition-all duration-300 hover:shadow-[-8px_8px_15px_rgba(239,68,68,0.3),_8px_8px_15px_rgba(239,68,68,0.3)] hover:scale-110 cursor-pointer'>
-            <div className='text-red-700 text-xl'> {icon}</div>
-            <p className='font-mono font-bold text-white text-lg'>{title}</p>
-            <p className='text-zinc-700 text-sm'>{description}</p>
+function SkillCard({ icon, title, description }) {
+  return (
+    <div className="bg-zinc-800 rounded-lg w-40 h-40 flex justify-center items-center flex-col gap-3 transition-all duration-300 hover:shadow-[-8px_8px_15px_rgba(239,68,68,0.3),_8px_8px_15px_rgba(239,68,68,0.3)] hover:scale-110 cursor-pointer flex-grow">
+      <div className="text-red-700 text-xl"> {icon}</div>
+      <p className="font-mono font-bold text-white text-lg">{title}</p>
+      <p className="text-zinc-700 text-sm ">{description}</p>
     </div>
-)
+  );
 }
 
-export default SkillCard
+export default SkillCard;
--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,19 +1,205 @@
-import React from 'react'
-import SkillCard from '../../components/SkillCard'
-import Header from '../../components/Header'
-import { FaNodeJs } from "react-icons/fa";
-
+import React from "react";
+import SkillCard from "../../components/SkillCard";
+import Header from "../../components/Header";
+import {
+  FaCss3Alt,
+  FaDocker,
+  FaHtml5,
+  FaJs,
+  FaNodeJs,
+  FaPython,
+  FaReact,
+} from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+import { DiMongodb, DiMysql } from "react-icons/di";
+import { SiMysql, SiGooglecloud } from "react-icons/si";
+import { FaC } from "react-icons/fa6";
+import { IoLogoVercel } from "react-icons/io5";
 
 function Skills() {
+  const skillsDataBack = [
+    {
+      id: 1,
+      icon: <FaNodeJs />,
+      title: "Node.js",
+      description: "Backend development",
+    },
+    {
+      id: 2,
+      icon: <IoLogoFirebase />,
+      title: "Firebase",
+      description: "Backend Development",
+    },
+  ];
+
+  const skillsDataFront = [
+    {
+      id: 1,
+      icon: <FaHtml5 />,
+      title: "HTML",
+      description: "Web Markup",
+    },
+    {
+      id: 2,
+      icon: <FaCss3Alt />,
+      title: "CSS",
+      description: "Frontend Styling",
+    },
+    {
+      id: 3,
+      icon: <FaJs />,
+      title: "JavaScript",
+      description: "Scripting Language",
+    },
+    {
+      id: 4,
+      icon: <FaReact />,
+      title: "React",
+      description: "Frontend Framework",
+    },
+  ];
+
+  const skillsData = [
+    {
+      id: 1,
+      icon: <DiMongodb />,
+      title: "Mongo DB",
+      description: "Non-Relational Database",
+    },
+    {
+      id: 2,
+      icon: <SiMysql />,
+      title: "MySQL",
+      description: "Relational Database",
+    },
+  ];
+
+  const skillsDataLang = [
+    {
+      id: 1,
+      icon: <FaC />,
+      title: "C",
+      description: "Coding Language",
+    },
+    {
+      id: 2,
+      icon: <FaPython />,
+      title: "Python",
+      description: "Coding Language",
+    },
+  ];
+
+  const skillsDataOther = [
+    {
+      id: 1,
+      icon: <FaDocker />,
+      title: "Docker",
+      description: "Container Management",
+    },
+    {
+      id: 2,
+      icon: <IoLogoVercel />,
+      title: "Vercel",
+      description: "Deployment Platform",
+    },
+    {
+      id: 3,
+      icon: <SiGooglecloud />,
+      title: "GCR",
+      description: "Google Cloud Run",
+    },
+  ];
+
   return (
     <div className="bg-[#141414] w-full min-h-screen">
-        <Header />
-        <div className=''>
-        <SkillCard icon={<FaNodeJs />} />
-        </div>
+      <Header />
+      <div className="pt-20 flex justify-center items-center flex-col">
+        <div className="w-full max-w-6xl px-4">
+          {/* Frontend Section */}
+          <div className="pt-2 flex justify-center items-center flex-col">
+            <p className="text-white font-[Bebas_Neue] text-2xl p-4 underline decoration-red-700 underline-offset-4">
+              Frontend
+            </p>
+            <div className="flex flex-wrap gap-8 p-4 w-full justify-center">
+              {skillsDataFront.map((skill) => (
+                <SkillCard
+                  key={skill.id}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                />
+              ))}
+            </div>
+          </div>
 
+          {/* Backend Section */}
+          <div className="pt-2 flex justify-center items-center flex-col">
+            <p className="text-white font-[Bebas_Neue] text-2xl p-4 underline decoration-red-700 underline-offset-4">
+              Backend
+            </p>
+            <div className="flex flex-wrap gap-8 p-4 w-full justify-center">
+              {skillsDataBack.map((skill) => (
+                <SkillCard
+                  key={skill.id}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="pt-2 flex justify-center items-center flex-col">
+            <p className="text-white font-[Bebas_Neue] text-2xl p-4 underline decoration-red-700 underline-offset-4">
+              Database
+            </p>
+            <div className="flex flex-wrap gap-8 p-4 w-full justify-center">
+              {skillsData.map((skill) => (
+                <SkillCard
+                  key={skill.id}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="pt-2 flex justify-center items-center flex-col">
+            <p className="text-white font-[Bebas_Neue] text-2xl p-4 underline decoration-red-700 underline-offset-4">
+              Other Languages
+            </p>
+            <div className="flex flex-wrap gap-8 p-4 w-full justify-center">
+              {skillsDataLang.map((skill) => (
+                <SkillCard
+                  key={skill.id}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="pt-2 flex justify-center items-center flex-col">
+            <p className="text-white font-[Bebas_Neue] text-2xl p-4 underline decoration-red-700 underline-offset-4">
+              Other Tools
+            </p>
+            <div className="flex flex-wrap gap-8 p-4 w-full justify-center">
+              {skillsDataOther.map((skill) => (
+                <SkillCard
+                  key={skill.id}
+                  icon={skill.icon}
+                  title={skill.title}
+                  description={skill.description}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Skills
+export default Skills;
